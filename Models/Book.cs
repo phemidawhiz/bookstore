cd66864f3082f5c9bbd4c51ab313b22676254498
unknown <feminiscence@gmail.com>
2024-05-28 14:30:20 +0100

    BREAKING CHANGE(Initial Configuration and Startup Code)

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public string isbn { get; set; }
+        public string author { get; set; }
+        public string year { get; set; }
+    }
+}
